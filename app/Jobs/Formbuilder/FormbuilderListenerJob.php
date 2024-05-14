b7b0b65b0ee172ed18f89dd28d8f5f3c3d272fac
kingsley achumie <kingsonly13c@gmail.com>
2024-05-14 18:01:22 +0100

    created-job-for-getting-data-from-formbuilder

--- app/Jobs/Formbuilder/FormbuilderListenerJob.php
+++ app/Jobs/Formbuilder/FormbuilderListenerJob.php
@@ -8,14 +8,16 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 
+
+/**
+ * @description this job is responsible for getting feedback from form builder service .
+ */
 class FormbuilderListenerJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
     public $data;
-    /**
-     * Create a new job instance.
-     */
+
     public function __construct(array $data)
     {
         $this->data = $data;
