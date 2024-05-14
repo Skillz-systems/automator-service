4a233e5d79066c5bb34960546609f0a7ba85fc13
kingsley achumie <kingsonly13c@gmail.com>
2024-05-14 17:12:54 +0100

    wip

--- app/Jobs/Formbuilder/FormbuilderListenerJob.php
+++ app/Jobs/Formbuilder/FormbuilderListenerJob.php
@@ -12,12 +12,13 @@ class FormbuilderListenerJob implements ShouldQueue
 {
     use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
+    public $data;
     /**
      * Create a new job instance.
      */
-    public function __construct()
+    public function __construct(array $data)
     {
-        //
+        $this->data = $data;
     }
 
     /**
--- tests/Feature/AutomatorJobsTest.php
+++ tests/Feature/AutomatorJobsTest.php
@@ -3,18 +3,19 @@
 namespace Tests\Feature;
 
 use App\Jobs\Automator\AutomatorTaskBroadcasterJob;
+use App\Jobs\Formbuilder\FormbuilderListenerJob;
 use App\Jobs\ProcessflowServiceJobs\ProcessflowHistoryJob;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Support\Facades\Queue;
 
-class ProcessFlowHistoryJobTest extends TestCase
+class AutomatorJobsTest extends TestCase
 {
     use RefreshDatabase;
 
     /**
-     * A basic feature test example.
+     * @description this test  is responsible for listening to ProcessflowHistoryJob and using the data to create a new task
      */
     public function test_it_handles_automation_task_creation_job(): void
     {
@@ -59,5 +60,31 @@ class ProcessFlowHistoryJobTest extends TestCase
         Queue::assertPushed(AutomatorTaskBroadcasterJob::class, function ($job) use ($request) {
             return $job->data == $request;
         });
+    }
+
+    /**
+     * 
+     */
+    public function test_to_see_that_formbuilder_listener_job_can_listen_to_the_rite_data(): void
+    {
+        //Queue::fake();
+
+        $data = [
+            "formbuilder_data_id" => 1,
+            "user_id" => 1,
+            "processflow_id" => 1,
+            "processflow_step_id" => 1,
+            "title" => "just a title ",
+            "entity_id" => 1,
+            "entity_type" => "customer",
+            "document_id" => 1,
+            "document_model" => "DDQModel",
+            "status" => 1,
+
+        ];
+
+        $job = new FormbuilderListenerJob($data);
+        $job->handle();
+        $this->assertEquals($job->data, $data);
     }
 }
